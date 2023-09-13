9c96551288195dde089424503129b977e7f383e8
Vid Drobnic <vid@zerodays.dev>
2023-09-13 21:15:35 +0200

    more efficent environment

--- src/evaluate.rs
+++ src/evaluate.rs
@@ -1,4 +1,4 @@
-use std::collections::HashMap;
+use std::{cell::RefCell, collections::HashMap, rc::Rc};
 
 use thiserror::Error;
 
@@ -18,36 +18,89 @@ pub enum Error {
 
 pub type Result<T> = std::result::Result<T, Error>;
 
-#[derive(Debug, PartialEq, Clone)]
-pub struct Environment {
+#[derive(Debug, PartialEq)]
+struct EnvironmentInner {
     store: HashMap<String, Object>,
+    outer: Option<Rc<RefCell<EnvironmentInner>>>,
 }
+
+impl EnvironmentInner {
+    fn get(&self, name: &str) -> Option<Object> {
+        if let Some(obj) = self.store.get(name) {
+            return Some(obj.clone());
+        }
+
+        if let Some(outer) = &self.outer {
+            return outer.borrow().get(name);
+        }
+
+        None
+    }
+
+    fn set(&mut self, name: String, value: Object) {
+        self.store.insert(name, value);
+    }
+}
+
+impl Clone for EnvironmentInner {
+    fn clone(&self) -> Self {
+        let outer = match &self.outer {
+            Some(outer) => {
+                let outer_clone = (*outer.borrow()).clone();
+                Some(Rc::new(RefCell::new(outer_clone)))
+            }
+            None => None,
+        };
+
+        EnvironmentInner {
+            store: self.store.clone(),
+            outer,
+        }
+    }
+}
+
+#[derive(Debug, PartialEq)]
+pub struct Environment(Rc<RefCell<EnvironmentInner>>);
 
 impl Environment {
     pub fn new() -> Self {
-        Environment {
+        Environment(Rc::new(RefCell::new(EnvironmentInner {
             store: HashMap::new(),
-        }
+            outer: None,
+        })))
     }
 
-    pub fn get(&self, name: &str) -> Option<&Object> {
-        self.store.get(name)
+    pub fn get(&self, name: &str) -> Option<Object> {
+        let env = self.0.borrow();
+        env.get(name)
     }
 
     pub fn set(&mut self, name: String, value: Object) {
-        self.store.insert(name, value);
+        let mut env = self.0.borrow_mut();
+        env.set(name, value);
     }
 
     pub fn extend(&self) -> Environment {
-        Environment {
-            store: self.store.clone(),
-        }
+        Environment(Rc::new(RefCell::new(EnvironmentInner {
+            store: HashMap::new(),
+            outer: Some(self.0.clone()),
+        })))
     }
 }
 
 impl Default for Environment {
     fn default() -> Self {
         Self::new()
+    }
+}
+
+impl Clone for Environment {
+    /// Clones the environment and all of its values.
+    /// Acts as "clone by value" in contrast to the extend method
+    /// which acts as "clone by reference".
+    fn clone(&self) -> Self {
+        let env = (*self.0.borrow()).clone();
+        Environment(Rc::new(RefCell::new(env)))
     }
 }
 
